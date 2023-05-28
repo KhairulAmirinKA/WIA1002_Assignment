9694cba6b3181dda26479e593fe0d3346eb41b8c
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-05-28 23:49:59 +0800

    put main menu GUI

---
+++ WIA1002_Assignment/src/GUI/MainMenuController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author Khairul<Khairul Amirin UM>
+ */
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+
+
+
+public class MainMenuController {
+    
+    @FXML
+    private Pane pane;
+    
+    @FXML
+   private Button start;
+    
+    @FXML
+    private Button exit;
+    
+    public void initialize(){
+        
+    }
+}
