deb1933ede0dca8854e5e3475b003440ce09b361
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-05-31 00:13:49 +0800

    successful in linking main menu to map

--- WIA1002_Assignment/src/GUI/JojoGUI.java
+++ WIA1002_Assignment/src/GUI/JojoGUI.java
@@ -11,7 +11,7 @@ package GUI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
