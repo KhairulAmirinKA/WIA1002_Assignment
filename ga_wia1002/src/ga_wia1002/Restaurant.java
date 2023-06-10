044a79ceed5fa92cd9b9b8ec09fcf43157f3caad
xiaow <xiaow@LAPTOP-E7O6Q0QN>
2023-06-10 19:43:20 +0800

    updated 10/6 7.42pm -- add 1 more constructor and some methods

--- ga_wia1002/src/ga_wia1002/Restaurant.java
+++ ga_wia1002/src/ga_wia1002/Restaurant.java
@@ -7,10 +7,20 @@ public class Restaurant {
     private String name;
     private List<String> availableFoods;
     private List<String> customers=new ArrayList<>();
+    private String foodName;
 
     public Restaurant(String name, List<String> availableFoods) {
         this.name = name;
         this.availableFoods = availableFoods;
+    }
+    
+    public Restaurant(String foodName, String name){
+        this.name = name;
+        this.foodName = foodName;
+    }
+
+    public String getFoodName() {
+        return foodName;
     }
 
     public String getName() {
