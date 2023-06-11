4901ca940d29d25fe53786c760f71e94d5c101e7
xiaow <xiaow@LAPTOP-E7O6Q0QN>
2023-06-11 20:45:55 +0800

    done(add more sorting criteria, no more problem)

--- ga_wia1002/src/ga_wia1002/ProjectJOJOLandSystem.java
+++ ga_wia1002/src/ga_wia1002/ProjectJOJOLandSystem.java
@@ -10,11 +10,20 @@ public class ProjectJOJOLandSystem {
     public ProjectJOJOLandSystem() {
         residentManager = new ResidentManager();
         standManager = new StandManager();
+        residentManager.loadResidents("residents.csv", "stands.csv");
+        standManager.loadStands("stands.csv");
+
+        
+    }
+public static void main(String[] args) {
+        ProjectJOJOLandSystem jojoSystem = new ProjectJOJOLandSystem();
+        jojoSystem.displayResidentInformation("Polnareff Land");
+        System.out.println();
+        List<Resident> residents = residentManager.getResidentsByResidentialArea("Polnareff Land");
+        jojoSystem.sortResidents(residents, "name(asc);Stamina(desc);Precision(asc)");
     }
     
     public void displayResidentInformation(String currentLocation) {
-        residentManager.loadResidents("residents.csv", "stands.csv");
-        standManager.loadStands("stands.csv");
         List<Resident> residents = residentManager.getResidentsByResidentialArea(currentLocation);
         if (residents.isEmpty()) {
             System.out.println("No residents found in " + currentLocation);
@@ -61,34 +70,9 @@ public class ProjectJOJOLandSystem {
         System.out.println();
         }
 
-
-
-
-        public void viewResidentProfile(Resident selectedResident) {
-        System.out.println("Resident Profile:");
-        System.out.println("Name: " + selectedResident.getName());
-        System.out.println("Age: " + selectedResident.getAge());
-        System.out.println("Gender: " + selectedResident.getGender());
-
-
-        Stand stand = standManager.getStandByUser(selectedResident.getName());
-        if (stand != null) {
-            System.out.println("Stand Information:");
-            System.out.println("Stand: " + stand.getName());
-            System.out.println("Destructive Power: " + stand.getDestructivePower());
-            System.out.println("Speed: " + stand.getSpeed());
-            System.out.println("Range: " + stand.getRange());
-            System.out.println("Stamina: " + stand.getStamina());
-            System.out.println("Precision: " + stand.getPrecision());
-            System.out.println("Development Potential: " + stand.getDevelopmentPotential());
-        } else {
-            System.out.println("Stand information not available.");
-        }
-        }
-
     public static void sortResidents(List<Resident> residents, String sortingOrder) {
 
-          List<Resident> sortedResidents = new ArrayList<>(residents);
+        List<Resident> sortedResidents = new ArrayList<>(residents);
 
         if (sortedResidents.isEmpty()) {
             System.out.println("No residents to sort.");
@@ -103,7 +87,7 @@ public class ProjectJOJOLandSystem {
             sortingToken = sortingToken.trim();
 
             String[] sortingParts = sortingToken.split("\\(");
-            if (sortingParts.length < 2) {
+            if (sortingParts.length <2) {
                 System.out.println("Invalid sorting order: " + sortingOrder);
                 return;
                 }
@@ -114,36 +98,111 @@ public class ProjectJOJOLandSystem {
 
             Comparator<Resident> comparator = null;
 
-            switch (sortingField.toLowerCase()) {
-                case "name":
-                    comparator = Comparator.comparing(Resident::getName);
-                    break;
-                case "age":
-                    comparator = Comparator.comparingInt(Resident::getAge);
-                    break;
-                case "gender":
-                    comparator = Comparator.comparing(Resident::getGender);
-                    break;
-                case "stand":
-                    comparator = (r1, r2) -> {
-                        Stand s1 = standManager.getStandByUser(r1.getName());
-                        Stand s2 = standManager.getStandByUser(r2.getName());
-                        if (s1 != null && s2 != null) {
-                            return s1.getName().compareTo(s2.getName());
-                        } else if (s1 != null) {
-                            return -1;
-                        } else if (s2 != null) {
-                            return 1;
-                        } else {
-                            return 0;
-                        }
-                    };
-                    break;
-                default:
-                    System.out.println("Invalid sorting field: " + sortingField);
-                    return;
-            }
-
+        switch (sortingField.toLowerCase()) {
+           case "name":
+               comparator = Comparator.comparing(Resident::getName);
+               break;
+           case "age":
+               comparator = Comparator.comparingInt(Resident::getAge);
+               break;
+           case "gender":
+               comparator = Comparator.comparing(Resident::getGender);
+               break;
+           case "destructive power":
+               comparator = (r1, r2) -> {
+                   Stand s1 = standManager.getStandByUser(r1.getName());
+                   Stand s2 = standManager.getStandByUser(r2.getName());
+                   if (s1 != null && s2 != null) {
+                       return s1.getDestructivePower().compareTo(s2.getDestructivePower());
+                   } else if (s1 != null) {
+                       return -1;
+                   } else if (s2 != null) {
+                       return 1;
+                   } else {
+                       return 0;
+                   }
+               };
+               break;
+           case "speed":
+               comparator = (r1, r2) -> {
+                   Stand s1 = standManager.getStandByUser(r1.getName());
+                   Stand s2 = standManager.getStandByUser(r2.getName());
+                   if (s1 != null && s2 != null) {
+                       return s1.getSpeed().compareTo(s2.getSpeed());
+                   } else if (s1 != null) {
+                       return -1;
+                   } else if (s2 != null) {
+                       return 1;
+                   } else {
+                       return 0;
+                   }
+               };
+               break;
+           case "precision":
+               comparator = (r1, r2) -> {
+                   Stand s1 = standManager.getStandByUser(r1.getName());
+                   Stand s2 = standManager.getStandByUser(r2.getName());
+                   if (s1 != null && s2 != null) {
+                       return s1.getPrecision().compareTo(s2.getPrecision());
+                   } else if (s1 != null) {
+                       return -1;
+                   } else if (s2 != null) {
+                       return 1;
+                   } else {
+                       return 0;
+                   }
+               };
+               break;
+           case "range":
+               comparator = (r1, r2) -> {
+                   Stand s1 = standManager.getStandByUser(r1.getName());
+                   Stand s2 = standManager.getStandByUser(r2.getName());
+                   if (s1 != null && s2 != null) {
+                       return s1.getRange().compareTo(s2.getRange());
+                   } else if (s1 != null) {
+                       return -1;
+                   } else if (s2 != null) {
+                       return 1;
+                   } else {
+                       return 0;
+                   }
+               };
+               break;
+           case "stamina":
+               comparator = (r1, r2) -> {
+                   Stand s1 = standManager.getStandByUser(r1.getName());
+                   Stand s2 = standManager.getStandByUser(r2.getName());
+                   if (s1 != null && s2 != null) {
+                       return s1.getStamina().compareTo(s2.getStamina());
+                   } else if (s1 != null) {
+                       return -1;
+                   } else if (s2 != null) {
+                       return 1;
+                   } else {
+                       return 0;
+                   }
+               };
+               break;
+           case "potential":
+               comparator = (r1, r2) -> {
+                   Stand s1 = standManager.getStandByUser(r1.getName());
+                   Stand s2 = standManager.getStandByUser(r2.getName());
+                   if (s1 != null && s2 != null) {
+                       return s1.getDevelopmentPotential().compareTo(s2.getDevelopmentPotential());
+                   } else if (s1 != null) {
+                       return -1;
+                   } else if (s2 != null) {
+                       return 1;
+                   } else {
+                       return 0;
+                   }
+               };
+               break;
+           default:
+               System.out.println("Invalid sorting field: " + sortingField);
+               return;
+            }           
+            
             if (sortingDirection.equalsIgnoreCase("asc")) {
                 comparators.add(comparator);
             } else if (sortingDirection.equalsIgnoreCase("desc")) {
@@ -152,28 +211,30 @@ public class ProjectJOJOLandSystem {
                 System.out.println("Invalid sorting direction: " + sortingDirection);
                 return;
             }
-        }
+            comparators.add(comparator);
+               }
 
-        Comparator<Resident> finalComparator = comparators.stream().reduce(Comparator::thenComparing).orElse(null);
+               Comparator<Resident> finalComparator = comparators.stream().reduce(Comparator::thenComparing).orElse(null);
 
-        if (finalComparator != null) {
-            Collections.sort(sortedResidents, finalComparator);
-            displaySortedResidents(sortedResidents);
-        } else {
-            System.out.println("No valid sorting criteria found.");
-        }
-    }
+               if (finalComparator != null) {
+                   Collections.sort(sortedResidents, finalComparator);
+                   displaySortedResidents(sortedResidents);
+               } else {
+                   System.out.println("No valid sorting criteria found.");
+               }
+           }
+
 
     public static void displaySortedResidents(List<Resident> residents) {
         System.out.println("Sorted Resident Information");
-        System.out.println("+----+-----------------------+-----+--------+------------------+-----------------+---------+-------+---------+------------+----------------------+");
+        System.out.println("+----+-----------------------+-----+--------+------------------+-------------------+-------+-------+---------+-----------+-----------------------+");
         System.out.println("| No | Name                  | Age | Gender | Stand            | Destructive Power | Speed | Range | Stamina | Precision | Development Potential |");
-        System.out.println("+----+-----------------------+-----+--------+------------------+-----------------+---------+-------+---------+------------+-----------------------+");
+        System.out.println("+----+-----------------------+-----+--------+------------------+-------------------+-------+-------+---------+-----------+-----------------------+");
 
         int counter = 1;
         for (Resident resident : residents) {
             Stand stand = standManager.getStandByUser(resident.getName());
-            System.out.printf("| %-2d | %-21s | %-3s | %-6s | %-16s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s |\n",
+            System.out.printf("| %-2d | %-21s | %-3s | %-6s | %-16s | %-17s | %-5s | %-5s | %-7s | %-9s | %-21s |\n",
                     counter, resident.getName(), resident.getAge(), resident.getGender(),
                     stand != null ? stand.getName() : "N/A",
                     stand != null ? stand.getDestructivePower() : "N/A",
@@ -185,7 +246,7 @@ public class ProjectJOJOLandSystem {
             counter++;
         }
 
-        System.out.println("+----+-----------------------+-----+--------+------------------+-----------------+");
+        System.out.println("+----+-----------------------+-----+--------+------------------+-------------------+-------+-------+---------+-----------+-----------------------+");
         System.out.println();
     }
 
