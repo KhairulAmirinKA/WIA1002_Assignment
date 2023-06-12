2969ac58a3467a0c6fcc48d672f727bf3bd27a96
xiaow <xiaow@LAPTOP-E7O6Q0QN>
2023-06-12 18:11:01 +0800

    update make copy waitingList

--- ga_wia1002/src/ga_wia1002/SavageGardenRestaurant.java
+++ ga_wia1002/src/ga_wia1002/SavageGardenRestaurant.java
@@ -13,19 +13,20 @@ public class SavageGardenRestaurant extends JOJOLandsRestaurant {
     }}
 class SavageGardenRule {
     public static List<Resident> processOrder(List<Resident> waitingList, int currentDay) {
+         List<Resident> waitingListCopy = new ArrayList<>(waitingList);
          LinkedList<Resident> processedList = new LinkedList<>();
          int count = 1;
          int index = 1;
 
-         while (!waitingList.isEmpty()) {
+         while (!waitingListCopy.isEmpty()) {
              if (count == currentDay) {
                  // Add the removed person to the end of the processed list
-                 processedList.addLast(waitingList.remove(index - 1));
+                 processedList.addLast(waitingListCopy.remove(index - 1));
                  count = 1;
 
-                 if (index >= waitingList.size()) {
+                 if (index >= waitingListCopy.size()) {
                      // Reached the end of the waiting list, start over from the last person in reverse
-                     Collections.reverse(waitingList);
+                     Collections.reverse(waitingListCopy);
                      index = 1;
                  }
              } 
@@ -33,9 +34,9 @@ class SavageGardenRule {
                  count++;
                  index++;
 
-                 if (index >= waitingList.size()) {
+                 if (index >= waitingListCopy.size()) {
                      // Reached the end of the waiting list, start over from the first person
-                     Collections.reverse(waitingList);
+                     Collections.reverse(waitingListCopy);
                      index = 1;
                  }
              }
