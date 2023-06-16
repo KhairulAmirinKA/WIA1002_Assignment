177664b1936fc0fad40c5e89eeb0955e8465e2ab
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-06-17 01:39:30 +0800

    add viewTotalSales_Milagro mode

--- ga_wia1002/src/ga_wia1002/JOJOLandsRestaurant.java
+++ ga_wia1002/src/ga_wia1002/JOJOLandsRestaurant.java
@@ -308,7 +308,7 @@ public abstract class JOJOLandsRestaurant {
         return restaurantPrice;
     }
     
-    //total sales
+    //total and average sales
     public void viewTotalSales(String restaurant_name){
         Scanner sc= new Scanner(System.in);
         
@@ -455,121 +455,7 @@ public abstract class JOJOLandsRestaurant {
             
                 
     }// end of viewTotalSales
-    
-    //milagroMan mode 13/6. including modify and view sales milagro
-    public void MilagroMode(String restaurant_name,int currentDay){
-        
-        Scanner sc1= new Scanner(System.in);
-        
-        int choice=0;
-        
-        //list of choices
-        while (choice!=3){
-        System.out.println(restaurant_name+" (Milagro Man Mode)");
-        System.out.println("[1] Modify Food Prices");
-        System.out.println("[2] View Sales Information");
-        System.out.println("[3] Exit Milagro Man");
-        
-        System.out.print("Select: ");
-        choice= sc1.nextInt();
-        
-        //choice=3 exiting
-        if (choice==3){
-            System.out.println("Exiting Milagro Man Mode");
-            break;
-        }
-        
-        //temp
-        HashMap<String, Double> restaurantPrice= chooseHashMap(restaurant_name);
-        
-        
-        //modify food prices
-        if (choice==1){
-        //load hashmap of milagro
-         restaurantPrice= milagroManMap(restaurant_name);  }
-        
-        //view sales with milagro man mode
-        else if (choice==2){
-            
-        //read file
-        try{
-             
-          //location
-          String filePath="src\\ga_wia1002\\"+restaurant_name+" Day "+ currentDay+" Order.txt";
-          
-         //read
-         Scanner sc= new Scanner(new FileInputStream(filePath));
-         
-         //map to store the quantity of food
-         HashMap<String, Integer> foodQuantityMap= new HashMap<>();
-         
-         //read from txt
-         while (sc.hasNextLine()) {
-                String line = sc.nextLine();
-                
-                //split
-                String[] orderData = line.split("\\|");
-                
-                if (orderData.length >= 5) {
-                    
-                    //get food name
-                    String foodName = orderData[5].trim();
-                    
-                    //calculate the quantity of each food
-                    foodQuantityMap.put(foodName, foodQuantityMap.getOrDefault(foodName, 0) + 1);
-                    
-                    //get the price of food
-                    double price = restaurantPrice.getOrDefault(foodName, 0.00);
-                      
-                }//if
-            }//while
-         
-         //display food, quantity and price
-         System.out.println("===================================================================================================");
-         System.out.println("Restaurant: "+restaurant_name+" (Milagro Man Mode)");
-         System.out.println("Day "+currentDay+" Sales");
-         System.out.println("+-----------------------------------------------+-------------------+");
-         System.out.printf("%-48s| %-9s| %-11s\n","|Food", "Quantity", "Price  |");
-         System.out.println("+-----------------------------------------------+-------------------+");
-        
-         //calc total sales
-         double totalSales=0; 
-         
-         
-         // Displaying the food quantity
-        for (String food : foodQuantityMap.keySet()) {
-            
-            //get quantity
-            int quantity = foodQuantityMap.getOrDefault(food,0);
-            
-            //calculate price. quantity x price per unit  
-            double price = quantity * restaurantPrice.getOrDefault(food,0.0);
-            
-            //calc total sales
-            totalSales+= price;
-            
-            //display food, quantity and price
-            System.out.printf("| %-45s | %-8d | $%-6.2f|%n", food, quantity, price);
-           
-        }// loop food name and quantity
-        System.out.println("+-----------------------------------------------+-------------------+");
-                 
-        //display the total sales
-            System.out.printf("|%57s | $%.2f|\n", "Total Sales", totalSales);
-            
-        System.out.println("+-----------------------------------------------+-------------------+");
-
-            sc.close();
-     }
      
-     catch (FileNotFoundException fe){
-         fe.printStackTrace();
-     }
-        
-        } //choice==2
-    } //while choice
-        
-    } 
     
     //minimum sales
     public void viewMinimumSales(String restaurant_name){
@@ -1161,10 +1047,236 @@ public abstract class JOJOLandsRestaurant {
     }// end of viewKHighest
     
 
+    //milagroMan mode 13/6. including modify and view sales milagro
+    public void MilagroMode(String restaurant_name,int currentDay){
+        
+        Scanner sc1= new Scanner(System.in);
+        
+        int choice=0;
+        
+        //list of choices
+        while (choice!=3){
+        System.out.println(restaurant_name+" (Milagro Man Mode)");
+        System.out.println("[1] Modify Food Prices");
+        System.out.println("[2] View Sales Information");
+        System.out.println("[3] Exit Milagro Man");
+        
+        System.out.print("Select: ");
+        choice= sc1.nextInt();
+        
+        //choice=3 exiting
+        if (choice==3){
+            System.out.println("Exiting Milagro Man Mode");
+            break;
+        }
+        
+        //temp
+        HashMap<String, Double> restaurantPrice= chooseHashMap(restaurant_name);
+        
+        
+        //modify food prices
+        if (choice==1){
+        //load hashmap of milagro
+         restaurantPrice= milagroManMap(restaurant_name);  }
+        
+        //view sales with milagro man mode
+        else if (choice==2){
+            
+        System.out.println("Sales Information");
+            System.out.println("[1] View Sales");
+            System.out.println("[2] View Agrregated Information");
+            System.out.println("\t[A] Minimum Sales");
+            System.out.println("\t[B] Maximum Sales");
+            System.out.println("\t[C] Top k Highest Sales");
+            System.out.println("\t[D] Total and Average Sales");
+            System.out.println("[3] Exit");
+            System.out.print("\nSelect: ");
+            sc1.nextLine(); //to avoid input skipping
+            String select =sc1.nextLine();
+            //sc1.nextLine();
+            System.out.println("==========================================================================================================================");
+            
+            switch(select){
+                case("1"):
+                    //[1]view Sales
+                        System.out.print("Enter Day: ");
+                        int userInputDay= sc1.nextInt();
+                        //resInfo.viewSales(currentLocation.getName(),userInputDay);
+                        break;
+                case("2A"):
+                    //[2A] Minimum Sales
+                    //resInfo.viewMinimumSales(currentLocation.getName());
+                    break;
+                case("2B"):
+                    //[2B]Maximum Sales
+                    //resInfo.viewMaximumSales(currentLocation.getName());
+                    break;
+                case("2C"):
+                    //[2C]Top k Highest Sales
+                    //resInfo.view_K_Highest(currentLocation.getName());
+                    break;
+                case("2D"):
+                    //[2D]Total and Average Sales
+                    viewTotalSales_Milagro(restaurant_name, restaurantPrice);
+                    break;      
+                }//dwitch
+            
+        
+        
+        } //choice==2
+    } //while choice
+        
+    }//end of milagro mode
     
+  
     
-    
+    //total and average sales milagro
+    public void viewTotalSales_Milagro(String restaurant_name, HashMap<String,Double> restaurantPrice){
+        Scanner sc= new Scanner(System.in);
+        
+        //start and end day
+        int startDay=0 , endDay=0;
+        
+        System.out.println();
+        System.out.println(restaurant_name+" Total Sales");
+        
+        System.out.print("Enter Start Day: ");
+        startDay= sc.nextInt();
+        
+        System.out.print("Enter End Day: ");
+        endDay= sc.nextInt();
+        
+        //read file
+        try{
+            
+            //file to store the total order
+            String totalSaleFile= "src\\ga_wia1002\\"+restaurant_name+" Total Order.txt";
+         PrintWriter writer= new PrintWriter(new FileOutputStream(totalSaleFile));
+         
+         
+         //read all files from day 1 to end day
+            for (int i=startDay; i<= endDay; i++){
+                
+            //location to read.
+            //eg: Jade Garden Day 1 Order.txt
+          String filePath="src\\ga_wia1002\\"+restaurant_name+" Day "+ i+" Order.txt";
+          
+         //file scanner
+         Scanner fileSc= new Scanner(new FileInputStream(filePath));
+         
+         //loop to write
+         while( fileSc.hasNextLine()){
+             String line= fileSc.nextLine();
+             
+             //write to a new file: totalsales.txt
+             writer.println(line);
+             
+         }//while
+         
+         fileSc.close();
+       
+         
+            }//for
+            
+            writer.close();
+        }//try
+        
+        catch (FileNotFoundException fe){
+            fe.printStackTrace();
+        }  //end of write to file
+        
+        
+        
+        //display the total sales
+        
+        
+        //read file containing total order
+        try{
+             
+          //location
+          String filePath="src\\ga_wia1002\\"+restaurant_name+" Total Order.txt";
+          
+         //read
+         Scanner fileSc= new Scanner(new FileInputStream(filePath));
+         
+         //map to store the quantity of food
+         HashMap<String, Integer> foodQuantityMap= new HashMap<>();
+         
+         //read from txt
+         while (fileSc.hasNextLine()) {
+                String line = fileSc.nextLine();
+                
+                //split
+                String[] orderData = line.split("\\|");
+                
+                if (orderData.length >= 5) {
+                    
+                    //get food name
+                    String foodName = orderData[5].trim();
+                    
+                    //calculate the quantity of each food
+                    foodQuantityMap.put(foodName, foodQuantityMap.getOrDefault(foodName, 0) + 1);
+                    
+                    //get the price of food
+                    double price = restaurantPrice.getOrDefault(foodName, 0.00);
+                      
+                }//if
+            }//while
+         
+         
+         //display food, quantity and price
+         System.out.println("\nRestaurant: "+restaurant_name);
+         System.out.printf("Total and Average Sales (Day %d - %d)\n", startDay, endDay);
+         System.out.println("+-----------------------------------------------+-------------------+");
+         System.out.printf("%-48s| %-9s| %-11s\n","|Food", "Quantity", "Price  |");
+         System.out.println("+-----------------------------------------------+-------------------+");
+         
+        //calc total sales and average
+         double totalSales=0;
+         double averageSales=0;
+         
+         // Displaying the food quantity
+        for (String food : foodQuantityMap.keySet()) {
+            
+            //get quantity
+            int quantity = foodQuantityMap.getOrDefault(food,0);
+            
+            //calculate price. quantity x price per unit  
+            double price = quantity * restaurantPrice.getOrDefault(food,0.0);
+            
+            //calc total sales
+            totalSales+= price;
+            
+            //display food, quantity and price
+            System.out.printf("| %-45s | %-8d | $%-6.2f|%n", food, quantity, price);
+           
+        }// loop food name and quantity
+        System.out.println("+-----------------------------------------------+-------------------+");
+                 
+        //display the total sales
+            System.out.printf("|%57s | $%.2f|\n", "Total Sales", totalSales);
+            
+            //calc average sales
+            averageSales= totalSales/ (endDay-startDay+1);
+            
+            //display the average sales
+            System.out.printf("|%57s | $%.2f|\n", "Average Sales", averageSales);
+            
+        System.out.println("+-----------------------------------------------+-------------------+");
+
+            fileSc.close();
+     }
+     
+     catch (FileNotFoundException fe){
+         fe.printStackTrace();
+     }
+        
+                
+            
+                
+    }// end of viewTotalSales
 }
+
     
     
     
