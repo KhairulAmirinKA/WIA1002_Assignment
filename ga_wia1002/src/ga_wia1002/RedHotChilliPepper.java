78937cfc7103e5857326cc4f4b722e12dfbd0cac
xiaow <xiaow@LAPTOP-E7O6Q0QN>
2023-06-18 13:29:51 +0800

    updated

--- ga_wia1002/src/ga_wia1002/RedHotChilliPepper.java
+++ ga_wia1002/src/ga_wia1002/RedHotChilliPepper.java
@@ -1,94 +1,137 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ga_wia1002;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
-/**
- *
- * @author Abdul Hadi
- */
-class RedHotChilliPepper {
-    public void activateAbility() {
-        System.out.println("You have activated the ability of Red Hot Chilli Pepper!");
-        System.out.println("You can now manipulate electricity and control nearby electronic devices!");
-    }
-      List <com.sun.jdi.connect.spi.Connection> powercables;
-        private Iterable<Location> locations;
-        public RedHotChilliPepper(){
-            this.powercables = new ArrayList<>();
-        }
-        public void upgradePowerCables(List<Location> locations, int budget) {
-            for (Location source : locations) {
-                for (Location destination : locations) {
-                    if (source != destination) {
-                        List<com.sun.jdi.connect.spi.Connection> shortestPath = findShortestPath(source, destination);
-    //                        for (Connection connection : shortestPath) {
-    //                            if (connection.getType().equals("power cable") && !connection.isOpen()) {
-    //                                if (budget >= connection.leng) {
-    //                                    connection.requireUpgrade = true;
-    //                                    powercables.add(connection);
-    //                                    budget -= connection.leng;
-    //                            } else {
-    //                                // Lacks budget to upgrade the connection
-    //                                break;
-    //                            }
-    //                        }
-    //                    }
-                    }
-                }
+public class RedHotChilliPepper{
+
+    public static List<GraphEdge> Kruskal(List<GraphEdge> edges) { // implement kruskal's logic
+
+        // Sort edges by weight
+        Collections.sort(edges);
+
+        List<String> vertices = new ArrayList<>(); // to store vertices
+        List<Integer> parent = new ArrayList<>();// to store parent of vertex
+
+        // to store the edges of the graph
+        List<GraphEdge> kruskal = new ArrayList<>();
+
+        for (GraphEdge edge : edges) {
+            String vertex1 = edge.vertex1; // source
+            String vertex2 = edge.vertex2; // destination
+
+            int index1 = getIndex(vertices, vertex1);
+            int index2 = getIndex(vertices, vertex2);
+
+            // if vertex not found in the list
+            if (index1 == -1) {
+                vertices.add(vertex1);
+                parent.add(vertices.size() - 1);// adds to parent list as well
+                index1 = vertices.size() - 1;
+            }
+
+            if (index2 == -1) {
+                vertices.add(vertex2);
+                parent.add(vertices.size() - 1);
+                index2 = vertices.size() - 1;
+            }
+
+            // check if vertices belong to other vertices
+            int parent1 = find(parent, index1);
+            int parent2 = find(parent, index2);
+            if (parent1 != parent2) {
+                kruskal.add(edge);
+
+                // merge components
+                union(parent, parent1, parent2);
             }
         }
-        private List<com.sun.jdi.connect.spi.Connection> findShortestPath(Location source, Location destination) {
-            Map<Location, Integer> distances = new HashMap<>();
-        Map<Location, com.sun.jdi.connect.spi.Connection> previousConnections = new HashMap<>();
-        PriorityQueue<Location> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(distances::get));
 
-        for (Location location : locations) {
-            distances.put(location, Integer.MAX_VALUE);
-            previousConnections.put(location, null);
-        }
-
-        distances.put(source, 0);
-        priorityQueue.offer(source);
-
-        while (!priorityQueue.isEmpty()) {
-            Location current = priorityQueue.poll();
-
-            if (current == destination) {
-                break;
-            }
-    //        for (Connection connection : current.connections) {
-    //            Location neighbor = connection;
-    //            int distance = distances.get(current) + connection.;
-    //            if (distance < distances.get(neighbor)) {
-    //                distances.put(neighbor, distance);
-    //                previousConnections.put(neighbor, connection);
-    //                priorityQueue.offer(neighbor);
-    //            }
-    //        }
-        }
-        List<com.sun.jdi.connect.spi.Connection> shortestPath = new ArrayList<>();
-        Location current = destination;
-        while (previousConnections.get(current) != null) {
-            com.sun.jdi.connect.spi.Connection connection = previousConnections.get(current);
-            shortestPath.add(connection);
-
-        }
-        Collections.reverse(shortestPath);
-        return shortestPath;
-        }
+        return kruskal;
     }
 
+    public static int getIndex(List<String> vertices, String vertex) {
+        for (int i = 0; i < vertices.size(); i++) {
+            if (vertices.get(i).equals(vertex)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    public static int find(List<Integer> parent, int vertex) {
+        // find the parent
+        if (parent.get(vertex) != vertex) { // check if have parent different from itself
+            parent.set(vertex, find(parent, parent.get(vertex))); // find all parents recursively
+        }
+        return parent.get(vertex);
+    }
 
+    public static void union(List<Integer> parent, int x, int y) {
+        parent.set(x, y); // set parent of vertex x to be vertex y
+    }
+
+    public void display() {
+            System.out.println("========================================================================================================");
+            List<GraphEdge> edges = new ArrayList<>();
+            edges.add(new GraphEdge("Town Hall", "Morioh Grand Hotel", 5));
+            edges.add(new GraphEdge("Town Hall", "Jade Garden", 5));
+            edges.add(new GraphEdge("Town Hall", "Cafe Deux Magots", 4));
+            edges.add(new GraphEdge("Morioh Grand Hotel", "Trattoria Trussardi", 6));
+            edges.add(new GraphEdge("Morioh Grand Hotel", "Jade Garden", 3));
+            edges.add(new GraphEdge("Jade Garden", "San Giorgio Maggiore", 2));
+            edges.add(new GraphEdge("Jade Garden", "Joestar Mansion", 2));
+            edges.add(new GraphEdge("Cafe Deux Magots", "Jade Garden", 3));
+            edges.add(new GraphEdge("Cafe Deux Magots", "Savage Garden", 4));
+            edges.add(new GraphEdge("Cafe Deux Magots", "Polnareff Land", 4));
+            edges.add(new GraphEdge("Savage Garden", "Polnareff Land", 6));
+            edges.add(new GraphEdge("Savage Garden", "Joestar Mansion", 4));
+            edges.add(new GraphEdge("Savage Garden", "Vineyard", 8));
+            edges.add(new GraphEdge("Vineyard", "Joestar Mansion", 3));
+            edges.add(new GraphEdge("Vineyard", "Libeccio", 6));
+            edges.add(new GraphEdge("Vineyard", "DIO's Mansion", 3));
+            edges.add(new GraphEdge("Libeccio", "Joestar Mansion", 6));
+            edges.add(new GraphEdge("Libeccio", "DIO's Mansion", 2));
+            edges.add(new GraphEdge("Libeccio", "Green Dolphin Street Prison", 3));
+            edges.add(new GraphEdge("Libeccio", "San Giorgio Maggiore", 4));
+            edges.add(new GraphEdge("Trattoria Trussardi", "San Giorgio Maggiore", 3));
+            edges.add(new GraphEdge("Trattoria Trussardi", "Green Dolphin Street Prison", 6));
+            edges.add(new GraphEdge("Angelo Rock", "DIO's Mansion", 3));
+            edges.add(new GraphEdge("Angelo Rock", "Green Dolphin Street Prison", 2));
+
+            System.out.println("Necessary Power Cables to be Upgraded: ");
+            List<GraphEdge> kruskal = Kruskal(edges);
+
+            int totalLength = 0;
+
+            for (int i = 0; i < kruskal.size(); i++) {
+                GraphEdge edge = kruskal.get(i);
+                totalLength += edge.weight;
+                System.out.println((i + 1) + ". " + edge.vertex1 + " -- " + edge.vertex2 + "(" + edge.weight + "km)");
+            }
+            System.out.println();
+            System.out.println("Total length: " + totalLength + "km");
+            System.out.println("========================================================================================================");
+        }
     
+    public static void main(String[] args) {
+        RedHotChilliPepper hotChilli = new RedHotChilliPepper();
+        hotChilli.display();
+    }
+    }
+
+class GraphEdge implements Comparable<GraphEdge> {
+    String vertex1;
+    String vertex2;
+    int weight;
+
+    public GraphEdge(String vertex1, String vertex2, int weight) {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(GraphEdge other) {
+        return this.weight - other.weight;
+    }
+}
