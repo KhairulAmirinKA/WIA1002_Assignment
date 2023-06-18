6cf0a3a72d0725ae9de2f580062d53118ff2bab9
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-06-18 18:06:44 +0800

    exception handling in milagro man

--- ga_wia1002/src/ga_wia1002/JOJOLandsRestaurant.java
+++ ga_wia1002/src/ga_wia1002/JOJOLandsRestaurant.java
@@ -1042,8 +1042,17 @@ public abstract class JOJOLandsRestaurant {
         System.out.println("[2] View Sales Information");
         System.out.println("[3] Exit Milagro Man");
         
-        System.out.print("Select: ");
-        choice= sc1.nextInt();
+//        System.out.print("Select: ");
+//        choice= sc1.nextInt();
+
+        try {
+            System.out.print("Select: ");
+            choice = sc1.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a number.\n");
+            sc1.nextLine(); // consume the invalid input
+            continue; // restart the loop
+        }
         
         //choice=3 exiting
         if (choice==3){
